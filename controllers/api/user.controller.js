9c936795063241632b5fdc7e2e520f9b8ffb8a7c
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2024-01-23 19:14:40 +0700

    add user model and user controller

---
+++ controllers/api/user.controller.js
@@ -0,0 +1,4 @@
+const Validator = require('fastest-validator');
+const { user } = require('./../../models');
+const v = new Validator();
+var bcycrpt = require('bcryptjs');
---
+++ models/user.model.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('user', {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+            field: 'id'
+        },
+        name: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            field: 'name'
+        },
+        email:{
+            type: DataTypes.STRING(100),
+            allowNull: false,
+            unique: true,
+            field: 'email'
+        },
+        password:{
+            type: DataTypes.STRING(255),
+            allowNull: false,
+            field: 'password'
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            field: 'updated_at',
+            defaultValue: DataTypes.NOW
+        }
+    }, {
+        tableName: 'users',
+        underscored: true
+    });
+
+    return User;
+}
