ab3598b9d84638cfd123b81637987d30564fb355
Lukas Valentino <91643553+lukasvalentino971@users.noreply.github.com>
2024-01-23 10:41:58 +0700

    add data dummy

--- seeders/20240123033637-user-seeder.js
+++ seeders/20240123033637-user-seeder.js
@@ -1,25 +1,19 @@
 'use strict';
+var bcrypt = require('bcryptjs');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('users',[{
+      name: 'John Doe',
+      email: 'johndoe@test.com',
+      password: bcrypt.hashSync('password', 8),
+      created_at: new Date(),
+      updated_at: new Date(),
+    }], {});
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
